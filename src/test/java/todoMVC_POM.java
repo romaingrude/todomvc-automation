26cbaee2fbb2de86772185e4abff9fd6d1c580af
Romain GRUDE <romaingrude@yahoo.fr>
2023-11-23 14:11:10 +0000

    Adds more test and storageClear

--- src/test/java/todoMVC_POM.java
+++ src/test/java/todoMVC_POM.java
@@ -1,7 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,8 +17,6 @@ public class todoMVC_POM {
 
     @FindBy(className = "todo-count")
     public WebElement todoCount;
-
-    
 
     
     
@@ -45,6 +41,7 @@ public class todoMVC_POM {
     }
 
 
+    // Adds an item
     public void addTodo(String newTodoItem){
         wait.until(ExpectedConditions.elementToBeClickable(inputTodo));
         inputTodo.click();
@@ -52,8 +49,38 @@ public class todoMVC_POM {
         inputTodo.sendKeys(Keys.ENTER);
     }
 
+
+    // Returns number of items left
     public String getTodoCount(){
         return todoCount.getText();
+    }
+
+
+    // Clicks the toggle button to mark item as conmpleted
+    public void markItemAsCompleted(int item_number){
+        String locator = "li:nth-child(" + item_number + ") .toggle";
+       WebElement markComplete = driver.findElement(By.cssSelector(locator));
+       markComplete.click();
+    }
+
+    public void deleteItem(int item_number) throws InterruptedException {
+        WebElement hover = driver.findElement(By.cssSelector("li:nth-child(" + item_number + ") .view"));
+        // Creates action to be able to hover over the element
+        Actions actions = new Actions(driver);
+        actions.moveToElement(hover).perform();
+        WebElement deleteButton = driver.findElement(By.cssSelector("li:nth-child(" + item_number + ") .destroy"));
+        deleteButton.click();
+    }
+
+    public String itemName(int item_number){
+        String locator = "li:nth-child(" + item_number + ") .view";
+        WebElement itemName = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector(locator))));
+        return itemName.getText();
+    }
+
+    public void clearLocalStorage(){
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("window.localStorage.clear();");
     }
 
 
--- src/test/java/todoMVC_TEST.java
+++ src/test/java/todoMVC_TEST.java
@@ -1,18 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-
 
 
 import java.io.File;
@@ -25,6 +20,7 @@ public class todoMVC_TEST {
 
     private static todoMVC_POM todo;
 
+
     @BeforeAll
     public static void launchBrowser(){
         WebDriverManager.firefoxdriver().setup();
@@ -35,12 +31,23 @@ public class todoMVC_TEST {
     public void setupPage(){
         todo = new todoMVC_POM(driver);
         todo.navigate();
+        todo.reactVersion();
     }
+
+    @AfterEach
+    public void clearBrowserStorage(){
+        todo.clearLocalStorage();
+    }
+
+    @AfterAll
+    public static void closeBrowser(){
+        driver.quit();
+    }
+
 
     @Test
     @Order(1)
     void getTitle() throws IOException {
-        todo.reactVersion();
         assertEquals("React • TodoMVC", driver.getTitle());
         takeScreenshot(driver, "screenshots/test.png");
     }
@@ -48,7 +55,6 @@ public class todoMVC_TEST {
     @Test
     @Order(2)
     void addItem() throws IOException {
-        todo.reactVersion();
         todo.addTodo("An item");
         takeScreenshot(driver, "screenshots/addItem.png");
     }
@@ -56,15 +62,56 @@ public class todoMVC_TEST {
     @Test
     @Order(3)
     void one_item_left_status() throws IOException {
-        todo.reactVersion();
         todo.addTodo("First item");
         assertEquals("1 item left", todo.getTodoCount());
         takeScreenshot(driver, "screenshots/one_item_left_status.png");
     }
 
-    @AfterAll
-    public static void closeBrowser(){
-        driver.quit();
+    @Test
+    @Order(4)
+    void two_item_left_status() throws IOException {
+        todo.addTodo("First item");
+        todo.addTodo("Second item");
+        assertEquals("2 items left", todo.getTodoCount());
+        takeScreenshot(driver, "screenshots/two_items_left_status.png");
+    }
+
+
+    @Test
+    @Order(5)
+    void zero_item_left_status() throws IOException{
+        todo.addTodo("First item");
+        todo.addTodo("Second item");
+        assertEquals("2 items left", todo.getTodoCount());
+        todo.markItemAsCompleted(1);
+        assertEquals("1 item left", todo.getTodoCount());
+        todo.markItemAsCompleted(2);
+        assertEquals("0 items left", todo.getTodoCount());
+        takeScreenshot(driver, "screenshots/zero_items_left_status.png");
+    }
+
+    @Test
+    @Order(6)
+    void tick_and_untick() throws IOException {
+        todo.addTodo("First item");
+        assertEquals("1 item left", todo.getTodoCount());
+        todo.markItemAsCompleted(1);
+        assertEquals("0 items left", todo.getTodoCount());
+        todo.markItemAsCompleted(1);
+        assertEquals("1 item left", todo.getTodoCount());
+        takeScreenshot(driver, "screenshots/tick_and_untick.png");
+    }
+
+    @Test
+    @Order(7)
+    void delete_item() throws IOException, InterruptedException {
+        todo.addTodo("First item");
+        todo.addTodo("Second item");
+        todo.addTodo("Third item");
+        todo.deleteItem(2);
+        assertEquals("2 items left", todo.getTodoCount());
+        assertEquals("Third item", todo.itemName(2));
+        takeScreenshot(driver, "screenshots/delete_second_item.png");
     }
 
 
