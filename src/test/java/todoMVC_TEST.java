4522f0bb35493903e36919232d1119ef8547ecf8
Romain GRUDE <romaingrude@yahoo.fr>
2023-11-23 11:17:10 +0000

    First tests

--- src/test/java/todoMVC_TEST.java
+++ src/test/java/todoMVC_TEST.java
@@ -1,17 +1,50 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+import java.io.File;
+import java.io.IOException;
 
 public class todoMVC_TEST {
 
     private static WebDriver driver;
 
+    private static todoMVC_POM todo;
+
     @BeforeAll
     public static void launchBrowser(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    @BeforeEach
+    public void setupPage(){
+        todo = new todoMVC_POM(driver);
+    }
+
+    @Test
+    void getTitle(){
+        todo.navigate();
+        todo.reactVersion();
+        assertEquals("React • TodoMVC", driver.getTitle());
+    }
+
+    @Test
+    void testAdd() throws IOException {
+        todo.navigate();
+        todo.reactVersion();
+        todo.addTodo("First Item");
+        takeScreenshot(driver, "test.png");
     }
 
     @AfterAll
@@ -19,5 +52,11 @@ public class todoMVC_TEST {
         driver.quit();
     }
 
+
+    public void takeScreenshot(WebDriver driver, String path) throws IOException {
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(path);
+        FileUtils.copyFile(screenshotFile, DestFile);
+    }
 
 }
